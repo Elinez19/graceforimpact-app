86933e1da78ff5e40bf3b3512739b71422363c46
Elijah Ndenwa <elijahndenwa19@gmail.com>
2024-07-10 11:57:18 +0100

    Improved the UI of the Home, About, and What WhatWeDo Pages

--- components/Health/HealthThree.tsx
+++ components/Health/HealthThree.tsx
@@ -35,10 +35,11 @@ const sections: HealthData[] = [
   {
     imgSrc: "/images/board/board-img-5.svg",
     videoId: "L61p2uyiMSo",
-    title: "GENERAL HEALTH",
-    description: `Over 400 million people do not have access to essential health services and people across at least 37 low- and middle-income countries are pushed further into extreme poverty (earning less than $1.25/day) because of the lack of healthcare. 
-      
-      The world’s most disadvantaged people are consistently missing out on even the most basic services which poses as a major threat to the goal of eliminating extreme poverty.`,
+    title: "EYE HEALTH",
+    description: `39 million people in the world are blind, and a further 191 million are visually impaired. Blindness is most prevalent in developing countries where malnutrition, inadequate health and education services, poor water quality, and a lack of sanitation lead to a high incidence of eye disease.
+Cataracts remain the leading cause of blindness in middle and low-income countries. A cataract occurs when clumping of cells or protein causes a cloudy or opaque area in the transparent lens which obstructs light as it thickens. Cataracts are easily reversible; the surgical procedure costing between $25 and $250, taking as little as 10 minutes.
+Collectively, blindness and visual impairment costs developing countries around $49 billion per year in lost economic activity. Completely ending avoidable blindness is expected to inject $517.1 billion into the poorest economies over a decade. Following eye surgery, a large amount of people are able to return to the workforce to be a productive part of society. Those who are not working are able to perform essential tasks for daily living on their own, freeing their caretakers- often children- to return to work or school. In the developing world, for every dollar invested, there is a $4 economic gain.
+`,
     memberId: "2",
   },
   // Add more sections with memberId and videoId for each board member
