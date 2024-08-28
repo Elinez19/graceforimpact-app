102fdc3ecb1787a11c6a297c959b8d46902094b4
Elijah Ndenwa <elijahndenwa19@gmail.com>
2024-08-28 13:44:03 +0100

    Updated the content and UI

--- components/SuccessStories/index.tsx
+++ components/SuccessStories/index.tsx
@@ -64,15 +64,23 @@ const SuccessStories = () => {
     <div className="relative overflow-hidden bg-white py-16 space-y-24">
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
         <h1 className="text-4xl font-bold text-purple">
-        Grace’s Success Stories
+          Grace’s Success Stories
         </h1>
         <p className="mt-2 text-lg text-purple">
           Empowering young people to reach their full potential.
         </p>
       </div>
       {sections.map((section, index) => (
-        <div key={index} className="relative flex flex-col lg:flex-row items-center justify-between space-y-6 lg:space-y-0 lg:space-x-6 bg-gray-100 border border-gray-300 rounded-lg py-6 px-6">
-          <div className={`flex-1 ${index % 2 === 0 ? 'order-1 lg:order-2' : 'order-1 lg:order-1'}`}>
+        <div
+          key={index}
+          className="relative flex flex-col lg:flex-row items-center justify-between space-y-6 lg:space-y-0 lg:space-x-6 bg-gray-100 border border-gray-300 rounded-lg py-6 px-6"
+          style={{ minHeight: '300px' }}
+        >
+          <div
+            className={`flex-1 h-full flex items-stretch ${
+              index % 2 === 0 ? 'order-1 lg:order-2' : 'order-1 lg:order-1'
+            }`}
+          >
             <Image
               src={section.imgSrc}
               alt={section.title}
@@ -81,7 +89,11 @@ const SuccessStories = () => {
               className="object-cover rounded-lg"
             />
           </div>
-          <div className={`flex-1 ${index % 2 === 0 ? 'order-2 lg:order-1' : 'order-2 lg:order-2'}`}>
+          <div
+            className={`flex-1 h-full flex flex-col justify-center ${
+              index % 2 === 0 ? 'order-2 lg:order-1' : 'order-2 lg:order-2'
+            }`}
+          >
             <h2 className="text-3xl font-bold tracking-tight text-purple">
               {section.title}
             </h2>
@@ -92,7 +104,8 @@ const SuccessStories = () => {
               <Link
                 href="/blog/blog-details"
                 className="inline-block bg-purple px-6 py-2 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-purple hover:bg-purple/95 hover:ring-purple"
-                passHref>
+                passHref
+              >
                 Learn More
               </Link>
             </div>
@@ -109,4 +122,4 @@ const SuccessStories = () => {
   );
 };
 
-export default SuccessStories
+export default SuccessStories;
